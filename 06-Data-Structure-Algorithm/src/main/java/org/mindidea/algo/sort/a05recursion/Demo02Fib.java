f367460c33e56eb75add88ed8ecf5b38c774e440
tsingyun <imtsingyun@gmail.com>
2021-11-18 00:55:11 +0800

    Update Demo02Fib.java

--- 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/sort/a05recursion/Demo02Fib.java
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/sort/a05recursion/Demo02Fib.java
@@ -5,12 +5,29 @@ package org.mindidea.algo.sort.a05recursion;
  */
 public class Demo02Fib {
 	public static void main(String[] args) {
-
+		System.out.println(fib1(6));
+		System.out.println(fib2(6));
 	}
 
-	public static int fib(int n) {
+	public static int fib1(int n) {
+		if (n <= 2) {
+			return n;
+		}
+		return fib1(n - 1) + fib1(n - 2);
+	}
 
-
-		return 0;
+	public static int fib2(int n) {
+		if (n <= 2) {
+			return 1;
+		}
+		int first = 1;
+		int second = 1;
+		int result = 2;
+		for (int i = 3; i <= n; i++) {
+			first = second;
+			second = result;
+			result = first + second;
+		}
+		return result;
 	}
 }
