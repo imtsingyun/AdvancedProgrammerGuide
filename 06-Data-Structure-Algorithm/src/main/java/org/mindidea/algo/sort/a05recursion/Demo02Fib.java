abed8b96abae223377611860f1ef45bb05925512
Tsingyun <imtsingyun@gmail.com>
2021-11-18 12:07:46 +0800

    Update Demo02Fib.java

--- 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/sort/a05recursion/Demo02Fib.java
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/sort/a05recursion/Demo02Fib.java
@@ -5,29 +5,47 @@ package org.mindidea.algo.sort.a05recursion;
  */
 public class Demo02Fib {
 	public static void main(String[] args) {
-		System.out.println(fib1(6));
-		System.out.println(fib2(6));
+		System.out.println(fib1(5));
+		System.out.println(fib2(5));
+		System.out.println(fib3(5));
 	}
 
+	/**************************************************************************/
 	public static int fib1(int n) {
 		if (n <= 2) {
-			return n;
+			return 1;
 		}
 		return fib1(n - 1) + fib1(n - 2);
 	}
 
-	public static int fib2(int n) {
+	/**************************************************************************/
+	private static int fib2(int n) {
+		if (n <= 2) {
+			return 1;
+		}
+		int[] arr = new int[n];
+		arr[0] = arr[1]	= 1;
+		return fib2(arr, n);
+	}
+
+	private static int fib2(int[] arr, int n) {
+		if (arr[n - 1] == 0) {
+			arr[n - 1] = fib2(arr, n - 1) + fib2(arr, n - 2);
+		}
+		return arr[n - 1];
+	}
+
+	/**************************************************************************/
+	public static int fib3(int n) {
 		if (n <= 2) {
 			return 1;
 		}
 		int first = 1;
 		int second = 1;
-		int result = 2;
 		for (int i = 3; i <= n; i++) {
-			first = second;
-			second = result;
-			result = first + second;
+			second = first + second;
+			first = second - first;
 		}
-		return result;
+		return second;
 	}
 }
