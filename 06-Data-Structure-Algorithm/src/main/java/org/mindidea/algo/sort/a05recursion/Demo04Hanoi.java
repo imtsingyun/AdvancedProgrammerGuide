7916381341d776aa1c698cea3c7448ae01af26d3
Tsingyun <imtsingyun@gmail.com>
2021-11-18 22:39:40 +0800

    hanoi

---
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/sort/a05recursion/Demo04Hanoi.java
@@ -0,0 +1,11 @@
+package org.mindidea.algo.sort.a05recursion;
+
+/**
+ * 当 n = 1 时，将盘子从 A 移到 C
+ * 当 n > 1 时，分为以下几步：
+ * 1. 将 n-1 个盘子从 A 移到 B
+ * 2. 将编号为 n 的盘子从 A 移到 C
+ * 3. 将 n-1 个盘子从 B 移到 C
+ */
+public class Demo04Hanoi {
+}
