c5a78c87e3b49cc8b6168771a1b7f26f9c93757d
tsingyun <imtsingyun@gmail.com>
2021-11-21 17:10:25 +0800

    Create Demo01Pirate.java

---
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/sort/a07greedy/Demo01Pirate.java
@@ -0,0 +1,24 @@
+package org.mindidea.algo.sort.a07greedy;
+
+import java.util.Arrays;
+
+public class Demo01Pirate {
+	public static void main(String[] args) {
+		int[] weights = {3, 5, 4, 10, 7, 14, 2, 11};
+		Arrays.sort(weights);
+
+		int capacity = 30;
+		int weight = 0;
+		int count = 0;
+
+		for (int i = 0; i< weights.length && weight < capacity; i++) {
+			int newWeight = weight + weights[i];
+			if (newWeight <= capacity) {
+				weight = newWeight;
+				count++;
+				System.out.println(weights[i]);
+			}
+		}
+		System.out.println(count);
+	}
+}
