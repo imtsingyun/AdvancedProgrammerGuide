d44c605250234fb56aa9749068c27a1ee1a0842d
tsingyun <imtsingyun@gmail.com>
2021-11-28 20:39:33 +0800

    rename

---
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/sort/a09dynamicprogramming/Demo06LongestCommSubString.java
@@ -0,0 +1,16 @@
+package org.mindidea.algo.sort.a09dynamicprogramming;
+
+/**
+ * 最长公共子串（连续）
+ * ABCFEA, ACFEBE 公共子串是 CFE
+ *
+ * 假设 dp(i,j) 是以 str1[i-1]、str2[j-1] 结尾的最长公共子串长度
+ *
+ */
+public class Demo06LongestCommSubString {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/sort/a09dynamicprogramming/Demo07LongestCommSubString.java
+++
@@ -1,4 +0,0 @@
-package org.mindidea.algo.sort.a09dynamicprogramming;
-
-public class Demo07LongestCommSubString {
-}
