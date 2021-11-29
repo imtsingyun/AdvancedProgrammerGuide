7759213c63544268ba3e5eda3315d4fa88300077
Tsingyun <imtsingyun@gmail.com>
2021-11-29 12:26:19 +0800

    Update Solution.java

--- 06-Data-Structure-Algorithm/src/main/java/org/mindidea/leetcode/_00_1_TwoSum/Solution.java
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/leetcode/_00_1_TwoSum/Solution.java
@@ -2,6 +2,7 @@ package org.mindidea.leetcode._00_1_TwoSum;
 
 import java.util.*;
 
+@SuppressWarnings("DuplicatedCode")
 public class Solution {
 
 	public static void main(String[] args) {
@@ -17,7 +18,17 @@ public class Solution {
 			}
 			map.put(nums[i], i);
 		}
+		return new int[0];
+	}
 
+	private static int[] twoSum2(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (map.get(target - nums[i]) != null) {
+				return new int[]{map.get(target - nums[i]), i};
+			}
+			map.put(nums[i], i);
+		}
 		return new int[0];
 	}
 }
