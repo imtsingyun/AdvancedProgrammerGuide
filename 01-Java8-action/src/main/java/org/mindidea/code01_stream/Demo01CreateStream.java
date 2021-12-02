f6a680873cd4c88ea48ad2aeca22446c34eb8faa
tsingyun <imtsingyun@gmail.com>
2021-12-03 00:42:47 +0800

    Stream

---
+++ 01-Java8-action/src/main/java/org/mindidea/code01_stream/Demo01CreateStream.java
@@ -0,0 +1,36 @@
+package org.mindidea.code01_stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Demo01CreateStream {
+	public static void main(String[] args) {
+		m1();
+	}
+
+	/**
+	 * 1. 使用 Stream.of 方法创建 stream
+	 */
+	private static void m1() {
+		List<String> list = Arrays.asList("a", "b", "c", "d", "E");
+		Stream<List<String>> strStream = Stream.of(list);
+		strStream.forEach(System.out::print);
+		//=================================================
+		Stream<Integer> stream1 = Stream.of(1, 2, 3, 4, 5, 6);
+		stream1 = stream1.map(i->{
+			System.out.print(i + " multiply by 2 ");
+			return i*2;
+		});
+		stream1.forEach(System.out::println);
+		System.out.println("====================");
+	}
+
+	/**
+	 *
+	 */
+	private static void m2() {
+		Stream<Object> stream1 = Stream.builder().add(2).add(3).add(5).add(8).build();
+		Stream<String> stream2 = Stream.<String>builder().add("a").add("b").add("c").build();
+	}
+}
--- 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/a07dynamicprogramming2/Demo01ClimbStairs.java
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/a07dynamicprogramming2/Demo01ClimbStairs.java
@@ -2,14 +2,23 @@ package org.mindidea.algo.a07dynamicprogramming2;
 
 public class Demo01ClimbStairs {
 	public static void main(String[] args) {
-
+		System.out.println(climb(1));
+		System.out.println(climb(2));
+		System.out.println(climb(3));
+		System.out.println(climb(4));
+		System.out.println(climb(5));
+		System.out.println(climb(6));
+		System.out.println(climb(7));
 	}
 
 	private static int climb(int n) {
 		if (n <=2 ) return n;
 
 		int first = 1, second = 2;
-
-		return 0;
+		for (int i = 3; i <= n; i++) {
+			second = first + second;
+			first = second - first;
+		}
+		return second;
 	}
 }
