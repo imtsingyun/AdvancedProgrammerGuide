9e1a93783a973ee0ca16d9950e3271e9416fe20e
tsingyun <imtsingyun@gmail.com>
2021-12-07 11:06:00 +0800

    twosum

--- 06-Data-Structure-Algorithm/src/main/java/org/mindidea/leetcode/_00_1_TwoSum/Solution.java
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/leetcode/_00_1_TwoSum/Solution.java
@@ -1,13 +1,17 @@
 package org.mindidea.leetcode._00_1_TwoSum;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("DuplicatedCode")
 public class Solution {
 
 	public static void main(String[] args) {
-		int[] nums = new int[]{2,7,11,15};
+		int[] nums = new int[]{2, 7, 11, 15};
 		System.out.println(Arrays.toString(twoSum(nums, 9)));
+		System.out.println(Arrays.toString(twoSum2(nums, 9)));
+		System.out.println(Arrays.toString(twoSum3(nums, 9)));
 	}
 
 	public static int[] twoSum(int[] nums, int target) {
@@ -35,11 +39,11 @@ public class Solution {
 	private static int[] twoSum3(int[] nums, int target) {
 		Map<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < nums.length; i++) {
-			if (map.get(nums[i]) == null) {
-				map.put(nums[i], i);
-			} else {
-
+			if (map.get(target - nums[i]) != null) {
+				return new int[]{map.get(target - nums[i]), i};
 			}
+			map.put(nums[i], i);
 		}
+		return new int[0];
 	}
 }
