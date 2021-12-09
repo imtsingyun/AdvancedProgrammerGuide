fef39d437c5c146d78bfeba5e335cb3ec08e44d6
tsingyun <imtsingyun@gmail.com>
2021-12-10 00:26:13 +0800

    Update Singleton2.java

--- 02-Java-Concurrency/src/main/java/org/mindidea/gbfbcxj1/chap14/Singleton2.java
+++ 02-Java-Concurrency/src/main/java/org/mindidea/gbfbcxj1/chap14/Singleton2.java
@@ -3,6 +3,9 @@ package org.mindidea.gbfbcxj1.chap14;
 public final class Singleton2 {
 
 	private byte[] data = new byte[1024];
+
+	// volatile 保证可见性、有序性
+	// 此处保证其他线程在用 instance 时 instance 内部的所有初始化已经完成
 	private static volatile Singleton2 instance = null;
 
 	private Singleton2() {}
