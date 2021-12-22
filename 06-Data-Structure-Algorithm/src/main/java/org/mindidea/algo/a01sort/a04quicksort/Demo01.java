94f709c16433d25cb78a8e9647f3f7f9a7d0d896
tsingyun <imtsingyun@gmail.com>
2021-12-22 14:12:26 +0800

    快速排序

---
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/a01sort/a04quicksort/Demo01.java
@@ -0,0 +1,22 @@
+package org.mindidea.algo.a01sort.a04quicksort;
+
+import org.mindidea.algo.zcommon.Utils;
+
+public class Demo01 {
+	public static void main(String[] args) {
+		Integer[] nums = Utils.generateArr(100, 10);
+		sort(nums);
+		Utils.printArr(nums);
+	}
+
+	private static void sort(Integer[] nums) {
+		// 判断略
+		for (int begin = 1; begin < nums.length; begin++) {
+			int cur = begin;
+			while (cur > 0 && nums[cur] < nums[cur - 1]) {
+				Utils.swap(nums, cur, cur - 1);
+				cur--;
+			}
+		}
+	}
+}
