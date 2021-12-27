2909c27ec5758f0430645eb98f80d586fa0ae369
tsingyun <imtsingyun@gmail.com>
2021-12-27 23:59:08 +0800

    Create Demo07.java

---
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/a01sort/a02selection/Demo07.java
@@ -0,0 +1,23 @@
+package org.mindidea.algo.a01sort.a02selection;
+
+import org.mindidea.algo.zcommon.Utils;
+
+public class Demo07 {
+	public static void main(String[] args) {
+		Integer[] nums = Utils.generateArr(100, 10);
+		sort(nums);
+		Utils.printArr(nums);
+	}
+
+	private static void sort(Integer[] nums) {
+		for (int end = nums.length - 1; end > 0; end--) {
+			int maxIndex = 0;
+			for (int begin = 1; begin <= end; begin++) {
+				if (nums[begin] < nums[maxIndex]) {
+					maxIndex = begin - 1;
+				}
+			}
+			Utils.swap(nums, maxIndex, end);
+		}
+	}
+}
