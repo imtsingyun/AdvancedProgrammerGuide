099ecc4c9a8b1c0c92ee90c6fbfdb7535b10892f
tsingyun <imtsingyun@gmail.com>
2021-12-28 16:35:42 +0800

    heapsort

---
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/a01sort/a03heapsort/Demo06.java
@@ -0,0 +1,46 @@
+package org.mindidea.algo.a01sort.a03heapsort;
+
+import org.mindidea.algo.zcommon.Utils;
+
+public class Demo06 {
+	public static void main(String[] args) {
+		Integer[] nums = Utils.generateArr(1000, 15);
+		sort(nums);
+		Utils.printArr(nums);
+	}
+
+	private static void sort(Integer[] nums) {
+		heapify(nums);
+		int heapSize = nums.length;
+		while (heapSize > 1) {
+			--heapSize;
+			Utils.swap(nums, 0, heapSize);
+			shiftDown(nums, heapSize, 0);
+		}
+	}
+
+	private static void heapify(Integer[] nums) {
+		int lastIndex = nums.length - 1;
+		int lastParentIndex = (lastIndex - 1) >> 1;
+		for (int i = lastParentIndex; i >= 0; i--) {
+			shiftDown(nums, nums.length, i);
+		}
+	}
+
+	private static void shiftDown(Integer[] nums, int n, int i) {
+		int root = nums[i];
+		while ((i << 1) + 1 < n) {
+			int childIndex = (i << 1) + 1;
+			int rightIndex = childIndex + 1;
+			if (rightIndex < n && nums[rightIndex] > nums[childIndex]) {
+				childIndex = rightIndex;
+			}
+			if (root > nums[childIndex]) {
+				break;
+			}
+			nums[i] = nums[childIndex];
+			i = childIndex;
+		}
+		nums[i] = root;
+	}
+}
