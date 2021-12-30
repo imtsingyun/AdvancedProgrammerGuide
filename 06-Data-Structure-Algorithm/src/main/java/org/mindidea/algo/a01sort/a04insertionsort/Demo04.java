caa41b33c8dc952a99bbd883e69c93b8513d2478
tsingyun <imtsingyun@gmail.com>
2021-12-30 16:08:14 +0800

    Create Demo04.java

---
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/a01sort/a04insertionsort/Demo04.java
@@ -0,0 +1,23 @@
+package org.mindidea.algo.a01sort.a04insertionsort;
+
+import org.mindidea.algo.zcommon.Utils;
+
+public class Demo04 {
+	public static void main(String[] args) {
+		Integer[] nums = Utils.generateArr(100, 10);
+		test(nums);
+		Utils.printArr(nums);
+	}
+
+	private static void test(Integer[] nums) {
+		for (int begin = 1; begin < nums.length; begin++) {
+			int curIdx = begin;
+			int curVal = nums[curIdx];
+			while (curIdx > 0 && curVal < nums[curIdx - 1]) {
+				nums[curIdx] = nums[curIdx - 1];
+				curIdx--;
+			}
+			nums[curIdx] = curVal;
+		}
+	}
+}
