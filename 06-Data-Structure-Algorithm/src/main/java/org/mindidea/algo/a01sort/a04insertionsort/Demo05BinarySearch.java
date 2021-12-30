0f1401d19f110185e3b913f16c355b640138c7f5
tsingyun <imtsingyun@gmail.com>
2021-12-30 16:08:11 +0800

    Create Demo05BinarySearch.java

---
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/algo/a01sort/a04insertionsort/Demo05BinarySearch.java
@@ -0,0 +1,33 @@
+package org.mindidea.algo.a01sort.a04insertionsort;
+
+import org.mindidea.algo.zcommon.Utils;
+
+public class Demo05BinarySearch {
+	public static void main(String[] args) {
+		Integer[] nums = Utils.generateArr(100, 15);
+		sort(nums);
+		Utils.printArr(nums);
+	}
+
+	private static void sort(Integer[] nums) {
+		for (int begin = 1; begin < nums.length; begin++) {
+			Integer curVal = nums[begin];
+
+			int left = 0;
+			int right = begin;
+
+			while (left < right) {
+				int mid = ((right - left) >> 1) + left;
+				if (nums[begin] >= nums[mid]) {
+					left = mid + 1;
+				} else if (nums[begin] < nums[mid]) {
+					right = mid;
+				}
+			}
+			for (int j = begin; j > left; j--) {
+				nums[j] = nums[j - 1];
+			}
+			nums[left] = curVal;
+		}
+	}
+}
