e45c98a15f746c97ffef2b179ba08ce7b9b8c2d8
Tsing Yun <imtsingyun@gmail.com>
2022-03-29 21:25:40 +0800

    Update MainTest.java

--- 08-Patterns/src/main/java/org/mindidea/creation/factory/factorymethod/MainTest.java
+++ 08-Patterns/src/main/java/org/mindidea/creation/factory/factorymethod/MainTest.java
@@ -2,11 +2,11 @@ package org.mindidea.creation.factory.factorymethod;
 
 public class MainTest {
 	public static void main(String[] args) {
-		WulinMiniCarFactory f1 = new WulinMiniCarFactory();
+		AbstractCarFactory f1 = new WulinMiniCarFactory();
 		f1.newCar().run();
-		WulinVanCarFactory f2 = new WulinVanCarFactory();
+		AbstractCarFactory f2 = new WulinVanCarFactory();
 		f2.newCar().run();
-		WulinRacingCarFactory f3 = new WulinRacingCarFactory();
+		AbstractCarFactory f3 = new WulinRacingCarFactory();
 		f3.newCar().run();
 	}
 }
