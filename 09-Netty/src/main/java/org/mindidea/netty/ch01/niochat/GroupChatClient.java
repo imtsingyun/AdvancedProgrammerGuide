3518beff1dba3133b3a172553d288a2e389b39a2
Tsing Yun <imtsingyun@gmail.com>
2022-06-26 15:22:29 +0800

    group chat nio

---
+++ 09-Netty/src/main/java/org/mindidea/netty/ch01/niochat/GroupChatClient.java
@@ -0,0 +1,94 @@
+package org.mindidea.netty.ch01.niochat;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class GroupChatClient {
+	private final String HOST = "127.0.0.1";
+	private final int PORT = 19090;
+	private Selector selector;
+	private SocketChannel socketChannel;
+	private String username;
+
+	public GroupChatClient() {
+		try {
+			selector = Selector.open();
+
+			InetSocketAddress inetSocketAddress = new InetSocketAddress(HOST, PORT);
+			socketChannel = SocketChannel.open(inetSocketAddress);
+			socketChannel.configureBlocking(false);
+			socketChannel.register(selector, SelectionKey.OP_READ);
+			username = socketChannel.getLocalAddress().toString().substring(1);
+			System.out.println(username + " is ok...");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void sendInfo(String info) {
+		info = username + "sad: " + info;
+		try {
+			socketChannel.write(ByteBuffer.wrap(info.getBytes(StandardCharsets.UTF_8)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void readInfo() {
+		try {
+			int readChannels = selector.select();
+			if (readChannels > 0) {
+				Set<SelectionKey> selectionKeys = selector.selectedKeys();
+				Iterator<SelectionKey> iterator = selectionKeys.iterator();
+				while (iterator.hasNext()) {
+					SelectionKey key = iterator.next();
+					if (key.isReadable()) {
+						SocketChannel sc = (SocketChannel) key.channel();
+						ByteBuffer buff = ByteBuffer.allocate(1024);
+						sc.read(buff);
+						String msg = new String(buff.array());
+						System.out.println(msg.trim());
+					}
+				}
+			} else {
+				System.out.println("没有可用通道");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		GroupChatClient client = new GroupChatClient();
+		new Thread(() -> {
+			while (true) {
+				client.readInfo();
+				try {
+					try {
+						TimeUnit.SECONDS.sleep(3);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNextLine()) {
+			String str = scanner.nextLine();
+			client.sendInfo(str);
+		}
+	}
+}
---
+++ 09-Netty/src/main/java/org/mindidea/netty/ch01/niochat/GroupChatServer.java
@@ -0,0 +1,94 @@
+package org.mindidea.netty.ch01.niochat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.StandardCharsets;
+
+public class GroupChatServer {
+
+	private Selector selector;
+	private ServerSocketChannel listenChannel;
+	private static final int PORT = 19090;
+
+	public GroupChatServer() {
+		try {
+			selector = Selector.open();
+			listenChannel = ServerSocketChannel.open();
+			listenChannel.socket().bind(new InetSocketAddress(PORT));
+			listenChannel.configureBlocking(false);
+			listenChannel.register(selector, SelectionKey.OP_ACCEPT);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void listen() {
+		try {
+			while (true) {
+				int count = selector.select();
+				if (count > 0) {
+					for (SelectionKey selectedKey : selector.selectedKeys()) {
+						if (selectedKey.isAcceptable()) {
+							SocketChannel sc = listenChannel.accept();
+							sc.configureBlocking(false);
+							sc.register(selector, SelectionKey.OP_READ);
+							System.out.println(sc.getRemoteAddress() + " is online.");
+						}
+						if (selectedKey.isReadable()) {
+							readMsg(selectedKey);
+						}
+					}
+				} else {
+					System.out.println("waiting");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void readMsg(SelectionKey key) {
+		SocketChannel channel = null;
+		try {
+			channel = (SocketChannel) key.channel();
+			ByteBuffer allocate = ByteBuffer.allocate(1024);
+			int count = channel.read(allocate);
+			if (count > 0) {
+				String msg = new String(allocate.array());
+				System.out.println("from 客户端：" + msg);
+
+				// 转发消息
+				resendInfo(msg, channel);
+			}
+		} catch (Exception e) {
+			try {
+				assert channel != null;
+				System.out.println(channel.getRemoteAddress() + " is offline...");
+				key.cancel();
+				channel.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+	}
+
+	private void resendInfo(String msg, SocketChannel self) throws IOException {
+		System.out.println("转发消息：");
+		for (SelectionKey key : selector.keys()) {
+			Channel channel = key.channel();
+			if (channel instanceof SocketChannel && channel != self) {
+				SocketChannel dest = (SocketChannel) channel;
+				ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes(StandardCharsets.UTF_8));
+				dest.write(buffer);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		GroupChatServer groupChatServer = new GroupChatServer();
+		groupChatServer.listen();
+	}
+}
