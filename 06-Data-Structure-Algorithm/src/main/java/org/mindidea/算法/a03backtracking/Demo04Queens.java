5f39d7af072c0c4b5a4b126f65c3b2f946f4c5d8
tsingyun <imtsingyun@gmail.com>
2022-07-21 15:58:34 +0800

    Update Demo04Queens.java

--- 06-Data-Structure-Algorithm/src/main/java/org/mindidea/算法/a03backtracking/Demo04Queens.java
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/算法/a03backtracking/Demo04Queens.java
@@ -1,21 +1,42 @@
 package org.mindidea.算法.a03backtracking;
 
 public class Demo04Queens {
-	@Override
-	protected void finalize() throws Throwable {
-		super.finalize();
-	}
 
 	public static void main(String[] args) {
-
+		new Demo04Queens().init(8);
 	}
 
 	int[] cols;
 	int ways;
 
 	void init(int n) {
-
+		cols = new int[n];
+		place(0);
+		System.out.println(ways);
 	}
 
+	void place(int row) {
+		if (row == cols.length) {
+			ways++;
+			return;
+		}
+		for (int col = 0; col < cols.length; col++) {
+			if (isValid(col, row)) {
+				cols[row] = col;
+				place(row + 1);
+			}
+		}
+	}
 
+	boolean isValid(int col, int row) {
+		for (int i = 0; i < row; i++) {
+			if (cols[i] == col) {
+				return false;
+			}
+			if (row - i == Math.abs(cols[i] - col)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
