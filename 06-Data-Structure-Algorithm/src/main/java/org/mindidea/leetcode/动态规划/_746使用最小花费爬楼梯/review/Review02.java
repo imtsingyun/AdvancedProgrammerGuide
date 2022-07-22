64410144e0eed94cf5c4b378cee0b33a141090b1
tsingyun <imtsingyun@gmail.com>
2022-07-22 10:48:58 +0800

    Create Review02.java

---
+++ 06-Data-Structure-Algorithm/src/main/java/org/mindidea/leetcode/动态规划/_746使用最小花费爬楼梯/review/Review02.java
@@ -0,0 +1,18 @@
+package org.mindidea.leetcode.动态规划._746使用最小花费爬楼梯.review;
+
+public class Review02 {
+
+	public static void main(String[] args) {
+		int[] cost = new int[]{10, 15, 20};
+		System.out.println(new Review02().minCostClimbingStairs(cost));
+	}
+
+	public int minCostClimbingStairs(int[] cost) {
+		int[] dp = new int[cost.length + 1];
+		dp[0] = dp[1] = 0;
+		for (int i = 2; i <= cost.length; i++) {
+			dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+		}
+		return dp[cost.length];
+	}
+}
